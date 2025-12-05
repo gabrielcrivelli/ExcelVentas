23f50bee3a613eabab88474e3ca1c8e8a7b29e83
gabrielcrivelli <91565128+gabrielcrivelli@users.noreply.github.com>
2025-12-05 16:12:20 -0300

    Delete old analitycs/comparatives.py (3/5)

--- src/analitycs/comparatives.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def monthly_summary(df: pd.DataFrame) -> pd.DataFrame:
-    return (
-        df.groupby(["Año", "Mes", "Sucursal"], as_index=False)[["Cantidad", "ImporteTotal"]]
-        .sum()
-    )
